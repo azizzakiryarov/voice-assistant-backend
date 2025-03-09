33f74522d0ddc9e26c47e0dce96f9dc3034ad01d
azizzakiryarov <zakiryarov@hotmail.com>
2025-03-09 11:19:05 +0100

    Added simple SecurityConfig

---
+++ src/main/java/com/voiceassistant/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.voiceassistant.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable) // Disabling CSRF for simplicity
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
